aa7ba2677d2a91276cff30d68e7e093ae1a22079
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-10-23 14:48:28 +0200

    zad 1 done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -7,7 +7,9 @@ console.log();
 items.forEach((itemek) => {
   const header = itemek.querySelector("h2");
   console.log("Category: ", header.textContent);
+
   const testNum = itemek.querySelectorAll("ul > li");
   console.log("Elements: ", testNum.length);
+
   console.log();
 });
