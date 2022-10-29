7aa431feff1b94f0cfb58ee963b3e351de1e5f94
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-10-29 21:21:09 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+"use strict";
 
+const loginForm = document.querySelector(".login-form");
+
+const loginBtn = loginForm.querySelector("button");
+console.log(loginBtn);
+
+const formHandler = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("dzbanie jak ty chcesz sie zalogować bez maila czy hasła");
+  } else {
+    const loginData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(loginData);
+    event.currentTarget.reset();
+  }
+};
+
+loginBtn.addEventListener("submit", formHandler);
