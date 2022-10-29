2e8fe255a704084266fda99a29fd0526e74bdc8b
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-10-29 23:18:21 +0200

    task 9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,11 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const body = document.querySelector("body");
 const widgetSpan = document.querySelector(".color");
 const widgetBtn = document.querySelector(".change-color");
 
-const widgetHandler = (event) => {};
+const widgetHandler = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  let background = body.style.backgroundColor;
+  widgetSpan.textContent = background;
+};
 
 widgetBtn.addEventListener("click", widgetHandler);
